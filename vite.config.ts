999704bbc9e961ff31f2338f3629fa476c1e21b1
bino <bonoself@gmail.com>
2023-12-18 22:03:25 +0900

    chore: add unplugin sets

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from '@vant/auto-import-resolver';
-
+import VueRouter from 'unplugin-vue-router/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({ /* options */ }),
+    VueRouter({ /* options */ }),
     vue({
       script: {
         defineModel: true,
