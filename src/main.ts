9eea6d50ad84f7e9c915e446750c5c5bbd38e88d
bino <bonoself@gmail.com>
2024-01-16 19:03:28 +0900

    feat: import Icon component

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createVfm } from 'vue-final-modal';
 import { createHead } from '@unhead/vue';
+import { Icon } from '@iconify/vue';
 import App from './App.vue';
 import router from './router';
 import './main.scss';
@@ -11,7 +12,8 @@ const app = createApp(App)
   .use(createPinia())
   .use(createVfm())
   .use(createHead)
-  .use(router);
+  .use(router)
+  .component('Icon', Icon);
 
 router
   .isReady()
